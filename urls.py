f63a3eb83442a42362ed8d29051731e3d237ab82
Muhammad Saad <127174906+MuhammadSaad92@users.noreply.github.com>
2025-06-11 16:21:55 +0500

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.customer_form, name='customer_form'),
-    path('list/', views.customer_list, name='customer_list'),
-    path('edit/<int:pk>/', views.customer_edit, name='customer_edit'),
-    path('delete/<int:pk>/', views.customer_delete, name='customer_delete'),
-]
